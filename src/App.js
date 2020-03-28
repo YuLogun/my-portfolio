5009154a583d9b8924c8f45e1a21a278e5712ce6
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-03-28 12:44:07 +0400

    router changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 import { Home } from './Home';
 import { Header } from './Header';
 import { About } from './About';
@@ -7,14 +7,14 @@ import { Projects } from './Projects';
 
 function App() {
   return (
-    <BrowserRouter basename="/">
+    <HashRouter basename="/">
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/projects" component={Projects} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 

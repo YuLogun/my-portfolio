17e539ed9c7326074058cb606c2d48b5c2e14e81
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-03-28 12:45:02 +0400

    Updates

---
+++ precache-manifest.83126ac2fca2378862725c21f3831fe5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18562258d4fe213396bf14a5e4387f9b",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "1318d4c3c144bcba063f",
+    "url": "/my-portfolio/static/css/main.3f0521a8.chunk.css"
+  },
+  {
+    "revision": "998d4b5c7ebe49a2a2a7",
+    "url": "/my-portfolio/static/js/2.2092a8af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.2092a8af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1318d4c3c144bcba063f",
+    "url": "/my-portfolio/static/js/main.92d20983.chunk.js"
+  },
+  {
+    "revision": "6d7a7337562760299d40",
+    "url": "/my-portfolio/static/js/runtime-main.00bba678.js"
+  },
+  {
+    "revision": "2b13c159359fb8139f075bb4e0cffe0a",
+    "url": "/my-portfolio/static/media/avatar.2b13c159.png"
+  },
+  {
+    "revision": "ecd3317f04027c6ad71259d9f0498091",
+    "url": "/my-portfolio/static/media/eCommerce.ecd3317f.png"
+  },
+  {
+    "revision": "251341c155abc5af5421557e494e9c65",
+    "url": "/my-portfolio/static/media/img.251341c1.png"
+  },
+  {
+    "revision": "4f86ca59d402ea2c69083755bdc70e7b",
+    "url": "/my-portfolio/static/media/projectsAvatar.4f86ca59.png"
+  },
+  {
+    "revision": "d25df36f34207c71d332997f0554e411",
+    "url": "/my-portfolio/static/media/quoteScreen.d25df36f.png"
+  }
+]);

122a3739aaf099d516de71b28166b692e79dfdaa
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-04-04 12:16:00 +0400

    added some animation to my home page

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,14 @@ import './assets/Home.scss'
 
 export const Home = () => (
   <div className="home">
-    <h1 className="home_hi">Hi!</h1>
-    <h1 className="home_name">I'm Iuliia, a Frontend Developer </h1>
-    <h5>...and a JavaScript Padawan</h5>
+    <h1>
+      <span className="home_hi first">Hi!</span>
+      <br />
+      <span className="home_span second">I'm Iuliia, </span>
+      <span className="home_span third">a Frontend Developer </span>
+    </h1>
+    <span className="home_span fourth">...and a JavaScript Padawan</span>
+    <br />
     <img src={img} alt="super girl" className="image-container_img" />
   </div>
 )

05cc9d8d114624d3e143353bb02464ed52d5f377
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-04-27 19:13:42 +0400

    minor changes

--- src/Projects.js
+++ src/Projects.js
@@ -1,34 +1,53 @@
-import React from 'react'
-import './assets/Projects.scss'
-import projectAvatar from './assets/projectsAvatar.png'
-import quotePic from './assets/quoteAppScreen.png'
-import commercePic from './assets/eCommerceScreen.png'
+import React from "react";
+import "./assets/Projects.scss";
+import projectAvatar from "./assets/projectsAvatar.png";
+import quotePic from "./assets/quoteAppScreen.png";
+import commercePic from "./assets/eCommerceScreen.png";
 
 export const Projects = () => (
   <div className="projects">
     <h1 className="projects_title">Here are some of my latest projects...</h1>
-    <img src={projectAvatar} className="projects_avatar" alt="projects avatar" />
+    <img
+      src={projectAvatar}
+      className="projects_avatar"
+      alt="projects avatar"
+    />
     <div className="projects_container">
       <div className="projects_container__item">
         <div className="projects_container__pic__container">
-          <a target="_blank" href="https://terry-and-neil-quote-machine.herokuapp.com/" rel="noopener noreferrer">
-            <img src={quotePic} className="projects_container__item__avatar" alt="quoye machine project preview" />
+          <a
+            target="_blank"
+            href="https://terry-and-neil-quote-machine.herokuapp.com/"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={quotePic}
+              className="projects_container__item__avatar"
+              alt="quoye machine project preview"
+            />
           </a>
         </div>
         <div className="projects_container__item__text">
-          This is a quote machine - magic ball project. Ask my 2 favourite authors what they think.
+          This is a quote machine (a magic ball project). Ask my 2 favourite
+          authors what they think.
         </div>
       </div>
       <div className="projects_container__item">
         <div className="projects_container__pic__container">
           <a href="##">
-            <img src={commercePic} className="projects_container__item__avatar" alt="e-commerce project preview" />
+            <img
+              src={commercePic}
+              className="projects_container__item__avatar"
+              alt="e-commerce project preview"
+            />
           </a>
         </div>
         <div className="projects_container__item__text">
-          This is an e-commerce project. I've always been a big fan of geek culture, so making something like this with React was a great deal of fun.
+          This is an e-commerce project. I've always been a big fan of geek
+          culture, so making something like this with React was a great deal of
+          fun.
         </div>
       </div>
     </div>
   </div>
-)
+);

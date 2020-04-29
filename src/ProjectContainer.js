76f557a944aa443338c5f3ebc2fa607258b05d13
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-04-29 19:28:17 +0400

    added ProjectContainer component, fixed css code, changed projects preview pics, made other minor changes

---
+++ src/ProjectContainer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./assets/ProjectContainer.scss";
+
+const ProjectContainer = ({ imageUrl, description, projectLink, imgAlt }) => {
+  return (
+    <div className="projects_container__item">
+      <div className="projects_container__pic__container">
+        <a target="_blank" href={projectLink} rel="noopener noreferrer">
+          <img
+            src={imageUrl}
+            className="projects_container__item__avatar"
+            alt={imgAlt}
+          />
+        </a>
+      </div>
+      <div className="projects_container__item__text">{description}</div>
+    </div>
+  );
+};
+
+export default ProjectContainer;
--- src/Projects.js
+++ src/Projects.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./assets/Projects.scss";
 import projectAvatar from "./assets/projectsAvatar.png";
-import quotePic from "./assets/quoteAppScreen.png";
 import commercePic from "./assets/eCommerceScreen.png";
+import quoteMachinePic from "./assets/quoteMachine.png";
+import ProjectContainer from "./ProjectContainer";
 
 export const Projects = () => (
   <div className="projects">
@@ -13,41 +14,21 @@ export const Projects = () => (
       alt="projects avatar"
     />
     <div className="projects_container">
-      <div className="projects_container__item">
-        <div className="projects_container__pic__container">
-          <a
-            target="_blank"
-            href="https://terry-and-neil-quote-machine.herokuapp.com/"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={quotePic}
-              className="projects_container__item__avatar"
-              alt="quoye machine project preview"
-            />
-          </a>
-        </div>
-        <div className="projects_container__item__text">
-          This is a quote machine (a magic ball project). Ask my 2 favourite
-          authors what they think.
-        </div>
-      </div>
-      <div className="projects_container__item">
-        <div className="projects_container__pic__container">
-          <a href="##">
-            <img
-              src={commercePic}
-              className="projects_container__item__avatar"
-              alt="e-commerce project preview"
-            />
-          </a>
-        </div>
-        <div className="projects_container__item__text">
-          This is an e-commerce project. I've always been a big fan of geek
-          culture, so making something like this with React was a great deal of
-          fun.
-        </div>
-      </div>
+      <ProjectContainer
+        imageUrl={quoteMachinePic}
+        projectLink="https://terry-and-neil-quote-machine.herokuapp.com/"
+        imgAlt="quote machine project preview"
+        description="This is a quote machine (or, if you wish, a magic ball project)
+          project. Ask my 2 favourite authors what they think."
+      />
+      <ProjectContainer
+        imageUrl={commercePic}
+        projectLink="https://github.com/YuLogun/e-commerce"
+        imgAlt="e-commerce project preview"
+        description="This is an e-commerce project. I've always been a big fan of geek
+            culture, so making something like this with React is a great deal of
+            fun. The project is still in progress, but you can view it on Github."
+      />
     </div>
   </div>
 );
